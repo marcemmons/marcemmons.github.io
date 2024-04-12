9223eebc9c64e2b7c5832d99e11566ac8a439e86
marcemmons <166767528+marcemmons@users.noreply.github.com>
2024-04-12 13:46:12 -0700

    updates blog

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def update_blog(commit_message ='updates blog'):
     repo = Repo(PATH_TO_REPO)
     repo.git.add(all=True)
     repo.index.commit(commit_message)
-    origin = repo.remote(name='origin')
+    origin = repo.remote(name=' https://github.com/marcemmons/marcemmons.github.io.git')
     origin.push
 
 random_text_string = '<h2> Random text </h2>'
