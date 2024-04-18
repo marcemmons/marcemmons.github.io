f5af1440e6dca713c11c8d8ce919da7c31239c28
marcemmons <166767528+marcemmons@users.noreply.github.com>
2024-04-17 20:14:23 -0700

    updates blog

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import shutil
 from openai import *
 import os
 from git import *
@@ -19,13 +20,40 @@ def update_blog(commit_message ='updates blog'):
     repo.index.commit(commit_message)
     origin = repo.remote(name='origin')
     origin.push()
+
+def create_new_blog(title, content, cover_image):
+    cover_image = Path(cover_image)
+
+    files = len(list(PATH_TO_CONTENT.glob('*.html')))
+    new_title = f'{files+1}.html'
+    path_to_new_content = PATH_TO_CONTENT/new_title
+    shutil.copy(cover_image, PATH_TO_CONTENT)
+
+    if not os.path.exists(path_to_new_content):
+        #Write new file
+        with open(path_to_new_content, 'w') as f:
+            f.write('<!DOCTYPE html> \n')
+            f.write('<html>\n')
+            f.write('<head>\n')
+            f.write(f'<title> {title} </title>\n')
+            f.write('</head>\n')
+            f.write('<body>\n')
+            f.write("<img src = '{cover_image.name}' alt = 'Cover Image'> <br/> \n")
+            f.write(f'<h1>{title}</h1>')
+            f.write(content.replace('\n', '<br />\n'))
+            f.write('</body>')
+            f.write('</html>')
+            print('Blog Created')
+            return(path_to_new_content)
+    else:
+        raise FileExistsError('File already exists, ABORT!')
+    
+    path_to_new_content = create_new_blog('Test Title', 'pure gibberish and lots of it', 'c:\ecojohn.jpg')
     
 
-random_text_string = '<h2> ANOTHER Very Hopeful text string </h2>'
+random_text_string = '<h2> ANOTHER ONE </h2>'
 
 with open(PATH_TO_BLOG/'index.html', 'w' ) as f:
     f.write(random_text_string)
 
 update_blog()
-print(PATH_TO_BLOG)
-print(PATH_TO_REPO)
